a6ddea109e5a98ca9005835d8b1da90326ce106e
Ravi Kumar <ravi.kuamr@oplandtech.com>
2025-11-09 16:27:13 +0530

    Enhance CORS configuration to allow specific origins and improve error handling

--- server/server.js
+++ server/server.js
@@ -16,7 +16,25 @@ connectDB();
 const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+
+ //CORS configuration 
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS not allowed for this origin'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400, // cache preflight 24h
+  })
+);
 
 // --- API Routes ---
 app.use('/auth', authRoutes);
